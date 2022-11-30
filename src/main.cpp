02b48e8ec78c4394df76188f15db1679e0898260
m1cha1s <msulej04@gmail.com>
2022-12-01 00:24:33 +0100

    Basic scaffold complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,35 @@
 #include <stdio.h>
 
-int main() {
-  printf("Hello world!\n");
+#include <raylib.h>
+
+#define RAYGUI_IMPLEMENTATION
+#include <raygui.h>
+
+#define P_WIDTH 800
+#define P_HEIGHT 800
+#define P_FPS 60
+
+int main()
+{
+  InitWindow(P_WIDTH, P_HEIGHT, "thePresenter");
+
+  SetTargetFPS(P_FPS);
+
+  while (!WindowShouldClose())
+  {
+    BeginDrawing();
+    {
+      ClearBackground(RAYWHITE);
+      DrawText("Hello world!", 400, 400, 21, BLACK);
+
+      if (GuiButton((Rectangle){200, 200, 80, 30}, "Test Button"))
+      {
+        printf("[Test button] pressed\n");
+      }
+    }
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
 }
